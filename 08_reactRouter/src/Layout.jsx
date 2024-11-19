537a5d089da174034156b9010f3140888a4f645e
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-11-19 13:03:57 +0530

    react Router

--- 08_reactRouter/src/Layout.jsx
+++ 08_reactRouter/src/Layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
-export default function Layout() {
+function Layout() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
   )
 }
+
+export default Layout
