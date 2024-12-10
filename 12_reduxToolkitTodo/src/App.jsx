77f9c1bcea2db20d72d2ed0b3db44faf215086b4
Rohit Mondal <rohitmondal13112001@gmail.com>
2024-12-10 12:13:22 +0530

    redux

--- 12_reduxToolkitTodo/src/App.jsx
+++ 12_reduxToolkitTodo/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Learn about react redux</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
--- 12_reduxToolkitTodo/src/app/store.js
+++ 12_reduxToolkitTodo/src/app/store.js
@@ -1,3 +1,6 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../features/todo/todoSlice";
 
-const store = configureStore({})
+export const store = configureStore({
+  reducer: todoReducer,
+});
