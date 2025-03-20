d0ac48b3ef8fee7e7a9a01f06d1af483d7da831b
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-03-21 00:12:44 +0530

    redux toolkit

--- 12_reduxToolkitTodo/src/App.jsx
+++ 12_reduxToolkitTodo/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import Todos from "./components/Todo";
+
 function App() {
   return (
     <>
