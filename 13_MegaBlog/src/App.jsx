7840dc129b338c2bdabde8d0e6117ef24d814d02
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-04-02 11:16:43 +0530

    app

--- 13_MegaBlog/src/App.jsx
+++ 13_MegaBlog/src/App.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -26,7 +27,7 @@ function App() {
       <div className="w-full block">
         <Header />
         <main>
-          
+          {/* <Outlet/> */}
         </main>
         <Footer />
       </div>
